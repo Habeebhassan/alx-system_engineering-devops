093c18e97dc8a2ce8a74d6123e8be6d984367a51
habeeb hassan <bidehassan@gmail.com>
2024-06-20 02:18:26 -0700

    v3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,25 @@
 """
 Contains the number_of_subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    """returns the number of total subscribers"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
+        return 0
 
-    else:
-        return (0)
+    try:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+        return 0
+    except ValueError:
+        return 0
