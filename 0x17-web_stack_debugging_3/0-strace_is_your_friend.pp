35d85a6f917bb7b8ed6f8527d34976cbb0df891d
habeeb hassan <bidehassan@gmail.com>
2024-06-23 12:59:10 -0700

    added comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Ensure that the directory '/var/www/html' exists with specific permissions and ownership
+
 file { '/var/www/html':
   ensure => directory,
   mode   => '0755',
@@ -5,6 +7,7 @@ file { '/var/www/html':
   group  => 'www-data',
 }
 
+# Ensure that the file '/var/www/html/.htaccess' exists with specific content, permissions, and ownership
 file { '/var/www/html/.htaccess':
   ensure  => file,
   content => 'RewriteEngine On\nRewriteRule ^index\.html$ welcome.html',
@@ -13,6 +16,7 @@ file { '/var/www/html/.htaccess':
   group   => 'www-data',
 }
 
+# Ensure that the 'apache2' service is running and enabled at boot time
 service { 'apache2':
   ensure    => running,
   enable    => true,
