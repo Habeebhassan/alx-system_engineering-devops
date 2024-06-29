fd50fba7cf4259f2f64f1560b629a635becdceb3
habeeb hassan <bidehassan@gmail.com>
2024-06-29 07:50:08 -0700

    versio 1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Nginx open file limit configuration Maniest
+exec { 'nginx fix':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  provider => 'shell'
+}
